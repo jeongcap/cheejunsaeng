6146abc271c54d8b2261a125f93f1e31e7d63e95
cheebo <cheebobot@gmail.com>
2018-05-16 23:54:54 +0900

    init

---
+++ apiKey.js
@@ -0,0 +1,2 @@
+const API_KEY = '017d8883faed46fca094bbe707186a43';
+module.exports = API_KEY;
--- index.js
+++ index.js
@@ -1,117 +1,43 @@
-// dependencies
-'use strict';
 const express = require('express');
 const bodyParser = require('body-parser');
-const http = require('https');
-var unirest = require("unirest");
-let errorResposne = {
-    results: []
-};
-var port = process.env.PORT || 8080;
-// create serve and configure it.
-const server = express();
-server.use(bodyParser.json());
-server.post('/getMovies',function (request,response)  {
-    if(request.body.result.parameters['top-rated']) {
-        var req = unirest("GET", "https://api.themoviedb.org/3/movie/top_rated");
-            req.query({
-                "page": "1",
-                "language": "en-US",
-                "api_key": ""
-            });
-            req.send("{}");
-            req.end(function(res) {
-                if(res.error) {
-                    response.setHeader('Content-Type', 'application/json');
-                    response.send(JSON.stringify({
-                        "speech" : "Error. Can you try it again ? ",
-                        "displayText" : "Error. Can you try it again ? "
-                    }));
-                } else if(res.body.results.length > 0) {
-                    let result = res.body.results;
-                    let output = '';
-                    for(let i = 0; i<result.length;i++) {
-                        output += result[i].title;
-                        output+="\n"
-                    }
-                    response.setHeader('Content-Type', 'application/json');
-                    response.send(JSON.stringify({
-                        "speech" : output,
-                        "displayText" : output
-                    })); 
-                }
-            });
-    } else if(request.body.result.parameters['movie-name']) {
-     //   console.log('popular-movies param found');
-        let movie = request.body.result.parameters['movie-name'];
-        var req = unirest("GET", "https://api.themoviedb.org/3/search/movie");
-            req.query({
-                "include_adult": "false",
-                "page": "1",
-                "query":movie,
-                "language": "en-US",
-                "api_key": ""
-            });
-            req.send("{}");
-            req.end(function(res) {
-                if(res.error) {
-                    response.setHeader('Content-Type', 'application/json');
-                    response.send(JSON.stringify({
-                        "speech" : "Error. Can you try it again ? ",
-                        "displayText" : "Error. Can you try it again ? "
-                    }));
-                } else if(res.body.results.length > 0) {
-                let result = res.body.results[0];
-                let output = "Average Rating : " + result.vote_average + 
-                "\n Plot : " + result.overview + "url" + result.poster_path
-                    response.setHeader('Content-Type', 'application/json');
-                    response.send(JSON.stringify({
-                        "speech" : output,
-                        "displayText" : output
-                    }));
-                } else {
-                    response.setHeader('Content-Type', 'application/json');
-                    response.send(JSON.stringify({
-                        "speech" : "Couldn't find any deatails. :(  ",
-                        "displayText" : "Couldn't find any deatails. :(  "
-                    }));
-                }
-            });
+const http = require('http');
+const API_KEY = require('./apiKey');
 
-    } else if(request.body.result.parameters['popular-movies']) {    
-        var req = unirest("GET", "https://api.themoviedb.org/3/movie/popular");
-            req.query({
-                "page": "1",
-                "language": "en-US",
-                "api_key": ""
+const server = express();
+server.use(bodyParser.urlencoded({
+    extended: true
+}));
+
+server.use(bodyParser.json());
+
+server.post('webhook', (req, res) => {
+    const companyToSearch = req.body.result && req.body.result.parameters && req.body.result.parameters.company ? req.body.result.parameters.company : 'KT'; //못찾으면 기본값(대부)
+    const reqUrl = encodeURI(`http://www.saramin.co.kr/zf_user/search/company?searchword=${companyToSearch}&searchType=auto&go`);
+
+    http.get(reqUrl, (responseFromAPI) => {
+        let completeResponse = '';
+        responseFromAPI.on('data', (chunk) => {
+            completeResponse += chunk;
+        });
+        responseFromAPI.on('end', () => {
+            const company = JSON.parse(completeResponse);
+            let dataToSend = 'OK';
+
+            return res.json({
+                speech: dataToSend,
+                displayText: dataToSend,
+                source: ''
             });
-            req.send("{}");
-            req.end(function(res){
-                if(res.error) {
-                    response.setHeader('Content-Type', 'application/json');
-                    response.send(JSON.stringify({
-                        "speech" : "Error. Can you try it again ? ",
-                        "displayText" : "Error. Can you try it again ? "
-                    }));
-                } else {
-                    let result = res.body.results;
-                    let output = '';
-                    for(let i = 0; i < result.length;i++) {
-                        output += result[i].title;
-                        output+="\n"
-                    }
-                    response.setHeader('Content-Type', 'application/json');
-                    response.send(JSON.stringify({
-                        "speech" : output,
-                        "displayText" : output
-                    })); 
-                }
-            });
-    }
+        });
+    }, (error) => {
+        return res.json({
+            speech: 'Something went wrong!',
+            displayText: 'Something went wrong!',
+            source: ''
+        });
+    });
 });
-server.get('/getName',function (req,res){
-    res.send('Swarup Bam');
-});
-server.listen(port, function () {
+
+server.listen((process.env.PORT || 8000), () => {
     console.log("Server is up and running...");
 });
