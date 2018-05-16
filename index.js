701b32f175318dc2d2f6ff68c470b9befa30bc34
cheebo <cheebobot@gmail.com>
2018-05-16 23:16:52 +0900

    init

--- index.js
+++ index.js
@@ -1,129 +1,117 @@
-const express = require('express')
-const bodyParser = require('body-parser')
-const app = express()
-
-const REQUIRE_AUTH = true
-const AUTH_TOKEN = 'an-example-token'
-
-app.use(bodyParser.json())
-app.set('port', (process.env.PORT || 5000))
-
-app.post('/webhook', function (req, res) {
-    if (REQUIRE_AUTH) {
-        if (req.headers['auth-token'] !== AUTH_TOKEN) {
-          return res.status(401).send('Unauthorized')
-        }
-    }
-
-    if (!req.body || !req.body.result || !req.body.result.parameters) {
-        return res.status(400).send('Bad Request')
-    }
-    var userName = req.body.result.parameters['given-name']
-    var webhookReply = 'Hello ' + userName + '! Welcome from the webhook.'
-
-    // the most basic response
-    res.status(200).json({
-        source: 'webhook',
-        speech: webhookReply,
-        displayText: webhookReply
-    })
-}
-
-
-
-/*
-var cheerio = require('cheerio');
-var request = require('request');
-
-var url = 'http://www.saramin.co.kr/zf_user/search/company?searchword=%EC%82%BC%EC%84%B1%EC%A0%84%EC%9E%90&searchType=auto&go=';
-var company, talent;
-var popup_link = "http://www.saramin.co.kr";
-var notice_url_saram = "http://www.saramin.co.kr";
-var notice, notice_num, notice_url;
-
-
-request(url, function(error, response, html){
-     if (!error) {
-        var $ = cheerio.load(html);
-        
-        
-        $('.company_tit > .tit ').first().find('a').each(function(){
-            var popup_link_info = $(this);
-            var popup_link_text = popup_link_info.text();
-            var popup_link_javascript = (popup_link_info).attr('href');
-
-     //       console.log(popup_link_text + " : " + popup_link_javascript);
-
-            var js_array = popup_link_javascript.split("'");
-        
-    //        console.log(js_array[1]);
-            
-            popup_link += js_array[1];
-
-            console.log(popup_link);
-
-      })
-    
-
-            
-        request(popup_link, function(error, response, html){
-            if (!error) {
-                var $1 = cheerio.load(html);
-                //인재상일때
-                $1('.section > .company_history_list').each(function(){
-                    var talent_info = $1(this);
-                    talent = talent_info.text();
-                    console.log(talent);
-                })
-
-                //채용정보일때 몇건인지
-                $1('.r_noti').each(function(){
-                    var notice_num_info = $1(this);
-                    notice_num = notice_num_info.text();
-                    console.log(notice_num);
-                })
-                //채용정보 세부 내역 
-                $1('.jcard').each(function(){
-                    var notice_info = $1(this);
-                    notice = notice_info.text();
-                    console.log(notice);
-                })
-                // 세부 내역에서 채용 공고 내용 url 찾고,
-                $1('.j_tit > b ').first().find('a').each(function(){
-                    var notice_info_saram = $1(this);
-                    notice_url_saram += (notice_info_saram).attr('href');
-
-                    console.log(notice_url_saram);
-                    
-                })
-                
-                홈페이지 지원 사이트 클래스?> 접근이 안됨
-                request(notice_url_saram, function(error, response, html){
-                    if (!error) {
-                        console.log("gg");
-                        var $2 = cheerio.load(html);
-                        $2('.wrap_recruit_view ').each(function(){
-                            
-                            console.log("gg");
-
-
-                            var notice_url_info = $2(this);
-                            notice_url = (notice_url_info).attr('href');
-
-                           console.log(notice_url);
-                           
-                        })
+// dependencies
+'use strict';
+const express = require('express');
+const bodyParser = require('body-parser');
+const http = require('https');
+var unirest = require("unirest");
+let errorResposne = {
+    results: []
+};
+var port = process.env.PORT || 8080;
+// create serve and configure it.
+const server = express();
+server.use(bodyParser.json());
+server.post('/getMovies',function (request,response)  {
+    if(request.body.result.parameters['top-rated']) {
+        var req = unirest("GET", "https://api.themoviedb.org/3/movie/top_rated");
+            req.query({
+                "page": "1",
+                "language": "en-US",
+                "api_key": ""
+            });
+            req.send("{}");
+            req.end(function(res) {
+                if(res.error) {
+                    response.setHeader('Content-Type', 'application/json');
+                    response.send(JSON.stringify({
+                        "speech" : "Error. Can you try it again ? ",
+                        "displayText" : "Error. Can you try it again ? "
+                    }));
+                } else if(res.body.results.length > 0) {
+                    let result = res.body.results;
+                    let output = '';
+                    for(let i = 0; i<result.length;i++) {
+                        output += result[i].title;
+                        output+="\n"
                     }
-                });
-                
-            }
+                    response.setHeader('Content-Type', 'application/json');
+                    response.send(JSON.stringify({
+                        "speech" : output,
+                        "displayText" : output
+                    })); 
+                }
+            });
+    } else if(request.body.result.parameters['movie-name']) {
+     //   console.log('popular-movies param found');
+        let movie = request.body.result.parameters['movie-name'];
+        var req = unirest("GET", "https://api.themoviedb.org/3/search/movie");
+            req.query({
+                "include_adult": "false",
+                "page": "1",
+                "query":movie,
+                "language": "en-US",
+                "api_key": ""
+            });
+            req.send("{}");
+            req.end(function(res) {
+                if(res.error) {
+                    response.setHeader('Content-Type', 'application/json');
+                    response.send(JSON.stringify({
+                        "speech" : "Error. Can you try it again ? ",
+                        "displayText" : "Error. Can you try it again ? "
+                    }));
+                } else if(res.body.results.length > 0) {
+                let result = res.body.results[0];
+                let output = "Average Rating : " + result.vote_average + 
+                "\n Plot : " + result.overview + "url" + result.poster_path
+                    response.setHeader('Content-Type', 'application/json');
+                    response.send(JSON.stringify({
+                        "speech" : output,
+                        "displayText" : output
+                    }));
+                } else {
+                    response.setHeader('Content-Type', 'application/json');
+                    response.send(JSON.stringify({
+                        "speech" : "Couldn't find any deatails. :(  ",
+                        "displayText" : "Couldn't find any deatails. :(  "
+                    }));
+                }
+            });
 
-        });
-
-
-
-  }
-
+    } else if(request.body.result.parameters['popular-movies']) {    
+        var req = unirest("GET", "https://api.themoviedb.org/3/movie/popular");
+            req.query({
+                "page": "1",
+                "language": "en-US",
+                "api_key": ""
+            });
+            req.send("{}");
+            req.end(function(res){
+                if(res.error) {
+                    response.setHeader('Content-Type', 'application/json');
+                    response.send(JSON.stringify({
+                        "speech" : "Error. Can you try it again ? ",
+                        "displayText" : "Error. Can you try it again ? "
+                    }));
+                } else {
+                    let result = res.body.results;
+                    let output = '';
+                    for(let i = 0; i < result.length;i++) {
+                        output += result[i].title;
+                        output+="\n"
+                    }
+                    response.setHeader('Content-Type', 'application/json');
+                    response.send(JSON.stringify({
+                        "speech" : output,
+                        "displayText" : output
+                    })); 
+                }
+            });
+    }
 });
-
-*/)
+server.get('/getName',function (req,res){
+    res.send('Swarup Bam');
+});
+server.listen(port, function () {
+    console.log("Server is up and running...");
+});
